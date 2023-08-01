481a85efa4fb8dc2b9e2f2d1fb8b001ea0bf1320
stc-c22005 <c22005@suntech.jp>
2023-08-01 11:50:09 +0900

    Initial commit

---
+++ app/src/main/java/jp/suntech/c22005/bmicalculatorc005/SumDialogFragment.java
@@ -0,0 +1,23 @@
+package jp.suntech.c22005.bmicalculatorc005;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import androidx.fragment.app.DialogFragment;
+
+public class SumDialogFragment extends DialogFragment {
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setTitle("警告")
+                .setMessage("適切な肥満度を求めるには、6歳未満の場合はカウプ指数が、6歳から15歳まではローレル指数が使われます。本アプリはBMIのみに対応しています。")
+                .setPositiveButton("確認", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        //ボタンが押されても何もしない
+                    }
+                });
+
+        return builder.create();
+    }
+}
